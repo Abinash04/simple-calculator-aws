dc01795364ab87f9e0323c2256fe73095a122daa
Abinash Behera <Abinash.behera04@gmail.com>
2024-02-19 11:49:33 +0000

    added build folder

---
+++ build/precache-manifest.3fc5787a9f31c8ffd7cf2dd602b84dbb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8b9dbb850e825e9f61f3efa1c3be6ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f324861f0b9aee6cd3fe",
+    "url": "/static/css/main.91f2b1eb.chunk.css"
+  },
+  {
+    "revision": "67bedec8172a165148fd",
+    "url": "/static/js/2.4324a1b5.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/static/js/2.4324a1b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a9ee94ae2ff2ed0c4c1",
+    "url": "/static/js/3.315faf07.chunk.js"
+  },
+  {
+    "revision": "f324861f0b9aee6cd3fe",
+    "url": "/static/js/main.32050b93.chunk.js"
+  },
+  {
+    "revision": "7f6c69340999506dd79f",
+    "url": "/static/js/runtime-main.229c3441.js"
+  }
+]);
